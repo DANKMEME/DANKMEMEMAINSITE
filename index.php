17132f162c247a8114173f6b1714a523a26c5b4b
PACEMENACE <ctscriptwrite@gmail.com>
2015-03-10 11:50:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+    
+    <link rel="dankmemeico" href="http://i1.kym-cdn.com/entries/icons/original/000/016/958/dank.PNG">
+	<head>
+		<title>DANK MEME</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+        
+        
+    </head>
+       
+		<header>
+			<?php include("header.php"); ?>
+		</header>
+		<aside>
+			<?php include("link.php"); ?>
+		</aside>
+		<section>
+			<?php include("redirect.php"); ?>
+		</section>		
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>	
+	</body>
+</html>
